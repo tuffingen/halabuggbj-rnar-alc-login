054bc3c13d0633fead638b7aa68d19ed5347af46
tuffingen <isac.danielsson03@gmail.com>
2022-03-25 10:49:00 +0100

    ✨ bcrypt

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+require('dotenv').config();
+
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
+var secretRouter = require('./routes/secret');
+
+const nunjucks = require('nunjucks');
+const bcrypt = require('bcrypt');
+
+
+
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(bcrypt());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -23,5 +33,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
+app.use('/secret', secretRouter);
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+
 
 module.exports = app;
--- routes/secret.js
+++ routes/secret.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../database');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('secret', { title: 'Express' });
+
+router.get('/', async (req, res, next) => {
+  await pool.promise()
+    .query('SELECT * FROM user')
+    .then(([rows, fields]) => {
+      res.render('secret.njk', {
+        items: rows,
+        title: 'secret',
+        layout: 'layout.njk'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        tasks: {
+          error: 'Error getting tasks'
+        }
+      })
+    });
 });
 
 module.exports = router;
