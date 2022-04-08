572700aa40c5e3eb0e4722af96125388155196da
filipandersson3 <filip.andersson3@elev.ga.ntig.se>
2022-04-08 11:22:26 +0200

    edit och delete funkar

--- routes/secret.js
+++ routes/secret.js
@@ -50,12 +50,14 @@ router.post('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   const id = req.params.id;
+  const username = req.session.username;
   if (isNaN(id)) {
       res.redirect('/secret/');
   } else {
       await pool.promise()
-          .query('SELECT * FROM meeps JOIN user ON user.id = meeps.user_id WHERE meeps.id = ?', [id])
+          .query('SELECT meeps.*,user.name FROM meeps JOIN user ON user.id = meeps.user_id WHERE meeps.id = ?', [id])
           .then(([rows, fields]) => {
+            if (rows[0].name == username) {
               console.log(rows);
               let data = {
                   message: "He llo",
@@ -64,6 +66,10 @@ router.get('/:id', async (req, res, next) => {
                   items: rows,
                 };
                 res.render('meepedit.njk', data);
+            } else {
+              res.redirect('/secret/');
+            }
+              
           })
           .catch(err => {
               console.log(err);
@@ -76,4 +82,56 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
+router.post('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const body = req.body.body;
+  if (isNaN(id)) {
+      res.status(400).json({
+          videos: {
+              error: 'Bad request'
+          }
+      })
+  } else {
+      await pool.promise()
+      .query('UPDATE meeps SET body = ? WHERE id = ?', [body,id])
+      .then((response) => {
+          console.log(response);
+          if (response[0].affectedRows === 1) {
+              res.redirect('/secret');
+          } else {
+              res.status(400).redirect('/secret' + id);
+          }
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json({
+              videos: {
+                  error: 'Error posting meeps'
+              }
+          })
+      });
+  }
+});
+
+router.get('/:id/delete/', async (req, res, next) => {
+  const id = req.params.id;
+  if (isNaN(req.params.id)) {
+      res.redirect('/secret/');
+  } else {
+      await pool.promise()
+          .query('DELETE FROM meeps WHERE id = ?', [id])
+          .then((response) => {
+              console.log(response);
+              if (response[0].affectedRows === 1) {
+                  res.redirect('/secret');
+              } else {
+                  res.redirect('/secret/' + id);
+              }
+          })
+          .catch(err => {
+              console.log(err);
+              res.redirect('/secret/' + id);
+          });
+  }
+});
 module.exports = router;
